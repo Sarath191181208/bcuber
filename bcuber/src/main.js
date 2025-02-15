071d1ec5944cae112198c2cc483cb002cac5b977
srinivasas-zluri <srinivasa.s@zluri.com>
2025-02-15 06:16:45 +0530

    Add error handling for missing elements and update QiYiCubeController initialization

--- bcuber/src/main.js
+++ bcuber/src/main.js
@@ -4,10 +4,24 @@ import { QiYiCubeController } from './components/qiyi/cubeController'
 import { QIYI_CONFIG } from './components/qiyi/config'
 
 const qiyiConnectButton = document.querySelector('#qiyi-connect-btn')
-
 const cubeRenderDiv = document.querySelector('#cube')
+const toggleGizmosButton = document.querySelector('#toggle-gizmos-btn')
+
+if (!qiyiConnectButton) {
+  console.warn('No connect button found.')
+  throw new Error('No connect button found.')
+}
+
+if (!cubeRenderDiv) {
+  console.warn('No render div found.')
+  throw new Error('No render div found.')
+}
+
+// @ts-ignore HTMLElement vs Element
 const cube = new RubiksCubeComponent(cubeRenderDiv)
-const qiyiHandler = new QiYiCubeController(QIYI_CONFIG, cube)
+
+// @ts-ignore HTMLElement vs Element
+const qiyiHandler = new QiYiCubeController(QIYI_CONFIG, cube, cubeRenderDiv)
 
 cube.toggleGizmos(true)
 
